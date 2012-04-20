6a7f5d314ea51ac6f0afc95d9c6d5dd6c7baf97d
Oishi <oishi@TheServer>
2012-04-20 01:30:18 -0400

    CSS updates

--- config/side.php
+++ config/side.php
@@ -3,7 +3,14 @@ if(array_key_exists('side', $_SESSION['aSites']))
 {	echo '<ul id="sidelist">';
 	foreach ($_SESSION['aSites']['side'] as $key=>$val)
 	{
-		echo '<li><a href="?do=',$key,'">',$val[1],'</a></li>';
+		if($_GET['do']!= $key)
+		{
+			echo '<li><a href="?do=',$key,'">',$val[1],'</a></li>';
+		}
+		else
+		{
+			echo '<li><span id="currentp">',$val[1],'</span></li>';
+		}
 	}
 	echo '</ul>';
 }
--- config/top.php
+++ config/top.php
@@ -3,7 +3,14 @@ if(array_key_exists('top', $_SESSION['aSites']))
 {
 	foreach ($_SESSION['aSites']['top'] as $key=>$val)
 	{
-		echo '<a href="?do=',$key,'">',$val[1],'</a>';			
+		if($_GET['do'] != $key)
+		{
+			echo '<a href="?do=',$key,'">',$val[1],'</a>';
+		}
+		else
+		{
+		echo '<span id="currentp">',$val[1],'</span>';
+		}
 	}
 }
 ?>
