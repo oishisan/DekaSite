6932677e7fe07370e26de2afd81c67c322a461e2
Oishi <oishi@TheServer>
2012-04-22 20:47:31 -0400

    Rebirth system additions

--- pages/rebirth.php
+++ pages/rebirth.php
@@ -2,83 +2,72 @@
 requireExtras();
 if(isset($ini['Other']['rebirth']) && isset($ini['Other']['rebirth.location']))
 {
-	echo '<table><tr><th>Rebirth</th><th>Required level</th><th>Points acquired</th></tr>';
+	echo '<table><tr><th>Rebirth</th><th>Required level</th><th>Points acquired</th><th>Cost (D-Coins)</th></tr>';
 	$count = count($ini['Other']['rebirth']);
 	for($i = 0; $i < $count; $i++)
 	{
 		$rArray = explode(',',$ini['Other']['rebirth'][$i]);
-		echo '<tr><td>',entScape($i+1),'</td><td>',entScape($rArray[0]),'</td><td>',entScape($rArray[1]),'</td></tr>';
+		echo '<tr><td>',entScape($i+1),'</td><td>',entScape($rArray[0]),'</td><td>',entScape($rArray[1]),'</td><td>',entScape($rArray[2]),'</td></tr>';
 	}
 	echo '</table>';
 	if(isset($_POST['rebirth']) && !empty($_POST['rchar']))
 	{
-		$cQuery = msquery("select login_flag from account.dbo.user_profile where login_flag = '0' and user_no = '%s'", $_SESSION['user_no']);
+		$cQuery = msquery("select login_flag, (amount + free_amount) as total from account.dbo.user_profile left join cash.dbo.user_cash on cash.dbo.user_cash.user_no = account.dbo.user_profile.user_no where login_flag = '0' and account.dbo.user_profile.user_no = '%s' group by login_flag, amount, free_amount", $_SESSION['user_no']);
 		if(mssql_num_rows($cQuery) == 1)
 		{
+			$ccFetch = mssql_fetch_array($cQuery);
 			$cQuery = msquery("select character.dbo.user_character.character_no, byPCClass, wLevel, rebirth from character.dbo.user_character left join %s.dbo.rebirth on %s.dbo.rebirth.character_no = character.dbo.user_character.character_no where character_name = '%s' and user_no = '%s'", $ini['MSSQL']['extrasDB'], $ini['MSSQL']['extrasDB'],$_POST['rchar'], $_SESSION['user_no']);
 			if(mssql_num_rows($cQuery) == 1)
 			{
 				$cFetch = mssql_fetch_array($cQuery);
-				if($cFetch['rebirth'] == null)
+				if($cFetch['rebirth'] == null || $cFetch['rebirth'] < $count)
 				{
 					$rArray = explode(',',$ini['Other']['rebirth'][0]);
 					if($cFetch['wLevel'] >= $rArray[0])
 					{
-						if(!isset($_POST['loc']) || !ctype_digit($_POST['loc']) || $_POST['loc'] > (count($ini['Other']['rebirth.location']) - 1))
+						if($ccFetch['total'] != null && $ccFetch['total'] >= $rArray[2])
 						{
-							$_POST['loc'] = 0;
+							if(!isset($_POST['loc']) || !ctype_digit($_POST['loc']) || $_POST['loc'] > (count($ini['Other']['rebirth.location']) - 1))
+							{
+								$_POST['loc'] = 0;
+							}
+							$rLoc = explode(',',$ini['Other']['rebirth.location'][$_POST['loc']]);
+							msquery("UPDATE cash.dbo.user_cash SET amount = amount - '%s' where user_no = '%s'", $rArray[2], $_SESSION['user_no']);
+							if($cFetch['rebirth'] == null)
+							{
+								msquery("INSERT into %s.dbo.rebirth values ('%s','1')", $ini['MSSQL']['extrasDB'], $cFetch['character_no']);
+								$sQuery = msquery("SELECT wStr, wCon, wDex, wSpr, wLevel from character.dbo.user_character where character_no = 'DEKARON%s000001'", $cFetch['byPCClass']); 
+								$sFetch = mssql_fetch_array($sQuery);
+								msquery("UPDATE character.dbo.user_character SET wStr = '%s', wSpr = '%s', wCon = '%s', wDex = '%s', wLevel = '%s', wStatPoint = '%s', wPosX = '%s', wPosY = '%s', wMapIndex = '%s', dwExp = '0' where character_name = '%s'", $sFetch['wStr'], $sFetch['wSpr'], $sFetch['wCon'], $sFetch['wDex'], $sFetch['wLevel'], $rArray[1], $rLoc[1], $rLoc[2], $rLoc[0], $_POST['rchar']);
+							}
+							else
+							{
+								msquery("update %s.dbo.rebirth set rebirth = rebirth +1 where character_no = '%s'", $ini['MSSQL']['extrasDB'], $cFetch['character_no']);
+								$sQuery = msquery("SELECT wStr, wCon, wDex, wSpr, wLevel from character.dbo.user_character where character_no = 'DEKARON%s000001'", $cFetch['byPCClass']); 
+								$sFetch = mssql_fetch_array($sQuery);
+								$stats = 0;
+								for($i = 1; $i <= ($cFetch['rebirth']+1); $i++)
+								{
+									$rArray = explode(',',$ini['Other']['rebirth'][$i-1]);
+									$stats += $rArray[1];
+								}
+								msquery("UPDATE character.dbo.user_character SET wStr = '%s', wSpr = '%s', wCon = '%s', wDex = '%s', wLevel = '%s', wStatPoint = '%s', wPosX = '%s', wPosY = '%s', wMapIndex = '%s', dwExp = '0' where character_name = '%s'", $sFetch['wStr'], $sFetch['wSpr'], $sFetch['wCon'], $sFetch['wDex'], $sFetch['wLevel'], $stats, $rLoc[1], $rLoc[2], $rLoc[0], $_POST['rchar']);
+							}
+							if($ini['Other']['rebirth.SkillPoint'] == true)
+							{
+								msquery("UPDATE character.dbo.user_character SET wSkillPoint = '0' WHERE character_name = '%s'", $_POST['rchar']);
+							}
+							if($ini['Other']['rebirth.Skill'] == true)
+							{
+								msquery("DELETE FROM character.dbo.user_slot WHERE character_no = '%s'", $cFetch['character_no']);
+								msquery("DELETE FROM character.dbo.user_skill WHERE character_no = '%s'", $cFetch['character_no']);
+							}
+							echo 'Rebirth successful!<br>';
 						}
-						$rLoc = explode(',',$ini['Other']['rebirth.location'][$_POST['loc']]);
-						msquery("INSERT into %s.dbo.rebirth values ('%s','1')", $ini['MSSQL']['extrasDB'], $cFetch['character_no']);
-						$sQuery = msquery("SELECT wStr, wCon, wDex, wSpr, wLevel from character.dbo.user_character where character_no = 'DEKARON%s000001'", $cFetch['byPCClass']); 
-						$sFetch = mssql_fetch_array($sQuery);
-						msquery("UPDATE character.dbo.user_character SET wStr = '%s', wSpr = '%s', wCon = '%s', wDex = '%s', wLevel = '%s', wStatPoint = '%s', wPosX = '%s', wPosY = '%s', wMapIndex = '%s', dwExp = '0' where character_name = '%s'", $sFetch['wStr'], $sFetch['wSpr'], $sFetch['wCon'], $sFetch['wDex'], $sFetch['wLevel'], $rArray[1], $rLoc[1], $rLoc[2], $rLoc[0], $_POST['rchar']);
-						if($ini['Other']['rebirth.SkillPoint'] == true)
+						else
 						{
-							msquery("UPDATE character.dbo.user_character SET wSkillPoint = '0' WHERE character_name = '%s'", $_POST['rchar']);
+							echo 'You do not have enough coins to rebirth.<br>';
 						}
-						if($ini['Other']['rebirth.Skill'] == true)
-						{
-							msquery("DELETE FROM character.dbo.user_slot WHERE character_no = '%s'", $cFetch['character_no']);
-							msquery("DELETE FROM character.dbo.user_skill WHERE character_no = '%s'", $cFetch['character_no']);
-						}
-						echo 'Rebirth successful!<br>';
-					}
-					else
-					{
-						echo 'Character does not meet level requirements.<br>';
-					}
-				}
-				elseif($cFetch['rebirth'] < $count)
-				{
-					$rArray = explode(',',$ini['Other']['rebirth'][$cFetch['rebirth']]);
-					if($cFetch['wLevel'] >= $rArray[0])
-					{
-						if(!isset($_POST['loc']) || !ctype_digit($_POST['loc']) || $_POST['loc'] > (count($ini['Other']['rebirth.location']) - 1))
-						{
-							$_POST['loc'] = 0;
-						}
-						$rLoc = explode(',',$ini['Other']['rebirth.location'][$_POST['loc']]);
-						msquery("update %s.dbo.rebirth set rebirth = rebirth +1 where character_no = '%s'", $ini['MSSQL']['extrasDB'], $cFetch['character_no']);
-						$sQuery = msquery("SELECT wStr, wCon, wDex, wSpr, wLevel from character.dbo.user_character where character_no = 'DEKARON%s000001'", $cFetch['byPCClass']); 
-						$sFetch = mssql_fetch_array($sQuery);
-						$stats = 0;
-						for($i = 1; $i <= ($cFetch['rebirth']+1); $i++)
-						{
-							$rArray = explode(',',$ini['Other']['rebirth'][$i-1]);
-							$stats += $rArray[1];
-						}
-						msquery("UPDATE character.dbo.user_character SET wStr = '%s', wSpr = '%s', wCon = '%s', wDex = '%s', wLevel = '%s', wStatPoint = '%s', wPosX = '%s', wPosY = '%s', wMapIndex = '%s', dwExp = '0' where character_name = '%s'", $sFetch['wStr'], $sFetch['wSpr'], $sFetch['wCon'], $sFetch['wDex'], $sFetch['wLevel'], $stats, $rLoc[1], $rLoc[2], $rLoc[0], $_POST['rchar']);
-											if($ini['Other']['rebirth.SkillPoint'] == true)
-						{
-							msquery("UPDATE character.dbo.user_character SET wSkillPoint = '0' WHERE character_name = '%s'", $_POST['rchar']);
-						}
-						if($ini['Other']['rebirth.Skill'] == true)
-						{
-							msquery("DELETE FROM character.dbo.user_slot WHERE character_no = '%s'", $cFetch['character_no']);
-							msquery("DELETE FROM character.dbo.user_skill WHERE character_no = '%s'", $cFetch['character_no']);
-						}
-						echo 'Rebirth successful!<br>';
 					}
 					else
 					{
