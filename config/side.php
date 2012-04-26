54a4495a245271e3ed231d9706a6815131df3095
Oishi <oishi@TheServer>
2012-04-26 12:15:20 -0400

    CSS support

--- config/side.php
+++ config/side.php
@@ -1,15 +1,24 @@
 <?php
+/*
+CSS page specific IDs
+---------------------
+#l-*    Where * is use the page's do action to style that link
+
+CSS page specific classes
+-------------------------
+.currentp       The text of the current page
+*/
 if(isset($_SESSION['aSites']['side']) && array_key_exists('side', $_SESSION['aSites']))
 {	echo '<ul>';
 	foreach ($_SESSION['aSites']['side'] as $key=>$val)
 	{
 		if($_GET['do']!= $key)
 		{
-			echo '<li><a href="?do=',$key,'">',$val[1],'</a></li>';
+			echo '<li><a href="?do=',entScape($key),'" id="l-',entScape($key),'">',entScape($val[1]),'</a></li>';
 		}
 		else
 		{
-			echo '<li><span id="currentp">',$val[1],'</span></li>';
+			echo '<li><span class="currentp" id="l-',entScape($key),'">',entScape($val[1]),'</span></li>';
 		}
 	}
 	echo '</ul>';
--- config/top.php
+++ config/top.php
@@ -1,15 +1,24 @@
 <?php
+/*
+CSS page specific IDs
+---------------------
+#l-*	Where * is use the page's do action to style that link
+
+CSS page specific classes
+-------------------------
+.currentp	The text of the current page
+*/
 if(isset($_SESSION['aSites']['top']) && array_key_exists('top', $_SESSION['aSites']))
 {
 	foreach ($_SESSION['aSites']['top'] as $key=>$val)
 	{
 		if($_GET['do'] != $key)
 		{
-			echo '<a href="?do=',$key,'">',$val[1],'</a>';
+			echo '<a href="?do=',entScape($key),'" id="l-',entScape($key),'">',entScape($val[1]),'</a>';
 		}
 		else
 		{
-		echo '<span id="currentp">',$val[1],'</span>';
+		echo '<span class="currentp" id="l-',entScape($key),'">',entScape($val[1]),'</span>';
 		}
 	}
 }
